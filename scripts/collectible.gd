cd68557fce717c05c10e1e4eb3d2fd1734a650e7
Tom Glenn <tom.e.glenn@gmail.com>
2022-09-05 23:26:13 +0100

    Creating a collectible - adding the script

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -1,10 +1,22 @@
 extends Area2D
 
+@export var value = 10
+
+@onready var game = $"/root/World"
+@onready var sprite = $AnimatedSprite2d
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.body_entered.connect(_on_body_entered)
 
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		game.add_score(value)
+		sprite.play("collected")
+		sprite.animation_finished.connect(_on_animation_finished)
+
+func _on_animation_finished():
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
