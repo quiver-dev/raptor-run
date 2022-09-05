981fe18fe2fe182a0fd8e93cddc1a7cf320b92da
Tom Glenn <tom.e.glenn@gmail.com>
2022-09-05 23:06:31 +0100

    Moving the camera along with the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 
 @onready var sprite = $AnimatedSprite2d
 @onready var jump_sound = $JumpSound
+@onready var camera = $"/root/World/Camera2d"
 
 var active = true
 var jumps_remaining = 2
@@ -18,6 +19,9 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	
 	if active:
+		# Update the camera position
+		camera.position = position
+		
 		# Reset the player after landing
 		if was_jumping and is_on_floor():
 			was_jumping = false
