63ab66bcb8603c339cc67bae12bc15704d864c1e
Tom Glenn <tom.e.glenn@gmail.com>
2022-09-24 18:48:15 +0100

    Update change_scene to change_scene_to_file for Beta 1

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -9,7 +9,7 @@ func _ready():
 	exit_button.pressed.connect(_on_exit_button_pressed)
 
 func _on_start_button_pressed():
-	get_tree().change_scene("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_exit_button_pressed():
 	get_tree().quit()
